908c33eab22366a3ab5e72db2d3ea553b7f3dfd3
Artem_Greengo86 <greengo86@gmail.com>
2017-03-20 16:49:19 +0300

    +assets

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -18,7 +18,7 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        //    Normalizee
+        //    Normalize
         'css/normalize.css',
         //    Bootstrap
 //        'css/bootstrap.css',
