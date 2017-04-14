84d5a28da9807b9ef6937b9e0348f67a8aedb9da
Artem <greengo86@gmail.com>
2017-04-14 15:26:48 +0300

    +Last_games

--- views/site/index.php
+++ views/site/index.php
@@ -15,7 +15,7 @@ $this->title = 'Footballstat';
 
 ?>
 
-<section>
+<section xmlns="http://www.w3.org/1999/html">
     <div class="container">
         <div class="row intro-tables">
             <?php
@@ -39,6 +39,29 @@ $this->title = 'Footballstat';
     </div>
 </section>
 
+<section id="last-games">
+    <div class="container">
+        <ul class="nav nav-tabs nav-justified text-center">
+            <li class="active"><h5><a data-toggle="tab" href="#champ1">Испания</a></h5></li>
+            <li><h5><a data-toggle="tab" href="#champ2">Англия</a></h5></li>
+            <li><h5><a data-toggle="tab" href="#champ3">Германия</a></h5></li>
+        </ul>
+        <div class="tab-content">
+            <div id="champ1" class="tab-pane in active">
+                <h5>Испания</h5>
+                <p>Содержимое 1 панели...</p>
+            </div>
+            <div id="champ2" class="tab-pane fade">
+                <h5>Англия</h5>
+                <p>Содержимое 2 панели...</p>
+            </div>
+            <div id="champ3" class="tab-pane fade">
+                <h5>Германия</h5>
+                <p>Содержимое 3 панели...</p>
+            </div>
+        </div>
+    </div>
+</section>
 
 <section id="services" class="section section-padded">
     <div class="container">
@@ -47,16 +70,16 @@ $this->title = 'Footballstat';
             <h4 class="light muted">Самое интересное из Ведущих Европейских чемпионатов</h4>
         </div>
         <?php
-        //Подключаем и кешируем виджет StatWidget
-        //if ($this->beginCache('stat', ['duration' => $duration, 'dependency' => $dependency])) {
-        StatWidget::begin(['champ' => '1']);
-        StatWidget::end();
-        StatWidget::begin(['champ' => '2']);
-        StatWidget::end();
-        StatWidget::begin(['champ' => '3']);
-        StatWidget::end();
-        //    $this->endCache();
-        //}
+            //Подключаем и кешируем виджет StatWidget
+            //if ($this->beginCache('stat', ['duration' => $duration, 'dependency' => $dependency])) {
+            StatWidget::begin(['champ' => '1']);
+            StatWidget::end();
+            StatWidget::begin(['champ' => '2']);
+            StatWidget::end();
+            StatWidget::begin(['champ' => '3']);
+            StatWidget::end();
+            //    $this->endCache();
+            //}
         ?>
     </div>
 </section>
